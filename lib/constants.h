288123007618abebad3a37fab89500a265a4ffb2
Guillermo García <g.gmenendez@protonmail.com>
2024-05-25 20:36:35 +0200

    chore(combos): use thumb to activate combos and add slow release them

---
+++ lib/constants.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Layers
+#define DVO 0
+#define QWE 1
+#define FUN 2
+#define NUM 3
+#define SYM 4
+
+#define XXX &none
+#define ___ &trans
+// To show graphically which key is being held to activate a layer
+#define vvv &trans
+
+// CUA bindings for paste, copy, and cut
+#define U_PST &kp LS(INS)
+#define U_CPY &kp LC(INS)
+#define U_CUT &kp LS(DEL)
+
+#define COMBO_TIMEOUT 40
