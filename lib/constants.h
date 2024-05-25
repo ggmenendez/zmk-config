52d7d4afe834ac16f2abe975cd5f8c1c90f45b87
Guillermo García <g.gmenendez@protonmail.com>
2024-05-26 01:48:19 +0200

    chore: use a single keymap for both corne keyboards

--- lib/constants.h
+++ lib/constants.h
@@ -1,12 +1,17 @@
 #pragma once
 
+//-----------------------------------------------------------------------------
 // Layers
+//-----------------------------------------------------------------------------
 #define DVO 0
 #define QWE 1
 #define FUN 2
 #define NUM 3
 #define SYM 4
 
+//-----------------------------------------------------------------------------
+// Keys
+//-----------------------------------------------------------------------------
 #define XXX &none
 #define ___ &trans
 // To show graphically which key is being held to activate a layer
@@ -16,5 +21,3 @@
 #define U_PST &kp LS(INS)
 #define U_CPY &kp LC(INS)
 #define U_CUT &kp LS(DEL)
-
-#define COMBO_TIMEOUT 40
